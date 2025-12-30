24e196608ed58b8f3170b245fee4c4fd50c42cc8
agouliel <agouliel@gmail.com>
2025-12-30 13:32:13 +0200

    added quart auth

--- backend/src/backend/run.py
+++ backend/src/backend/run.py
@@ -1,7 +1,10 @@
 from quart import Quart
+from quart_auth import QuartAuth
 from backend.blueprints.control import blueprint as control_blueprint
 
 app = Quart(__name__)
 app.config.from_prefixed_env(prefix='TOZO')
+
+auth_manager = QuartAuth(app)
 
 app.register_blueprint(control_blueprint)
